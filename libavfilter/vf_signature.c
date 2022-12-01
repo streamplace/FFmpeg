d4614b265df07a5deeee21bbe700914cea394ceb
Ivan Poleshchuk <cyberj0g@gmail.com>
2022-12-01 19:12:45 +0400

    Set frame similarity threshold to reduce fast verification fails (go-livepeer issue #2654) (#30)

--- libavfilter/vf_signature.c
+++ libavfilter/vf_signature.c
@@ -665,7 +665,7 @@ static int compare_signbuffer(uint8_t* signbuf1, int len1, uint8_t* signbuf2, in
         .filename = NULL,
         .thworddist = 9000,
         .thcomposdist = 60000,
-        .thl1 = 116,
+        .thl1 = 150,
         .thdi = 0,
         .thit = 0.5,
         .streamcontexts = scontexts
